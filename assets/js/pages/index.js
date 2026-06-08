bf071201bcc4f67b2dce50788489eb2dda31b00e
CamilTech <milamilinha2009@gmail.com>
2026-06-07 21:47:05 -0300

    pré-finalização da homepage

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -4,11 +4,24 @@ let carousel = document.querySelector('.carousel')
 let list = carousel.querySelector('.list')
 let items = carousel.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
-let dots = indicator.querySelectorAll('ul li')
+let indicatorList = indicator.querySelector('ul')
+let dots = null
 
 let active = 0
 let firstPosition = 0
 let lastPosition = items.length - 1
+
+function createDots() {
+  indicatorList.innerHTML = ''
+  items.forEach((_, index) => {
+    let dot = document.createElement('li')
+    if (index === 0) dot.classList.add('active')
+    indicatorList.appendChild(dot)
+  })
+  dots = indicatorList.querySelectorAll('li')
+}
+
+createDots()
 
 function setSlider() {
   let itemOld = carousel.querySelector('.list .item.active')
@@ -17,8 +30,6 @@ function setSlider() {
   let dotsOld = indicator.querySelector('ul li.active')
   dotsOld.classList.remove('active')
   dots[active].classList.add('active')
-
-  indicator.querySelector('.number').innerHTML = '0' + (active + 1)
 }
 
 nextButton.onclick = () => {
